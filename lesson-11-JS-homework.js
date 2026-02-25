93f71b4ad5b20e95565a4ac1e3f9ea4c7fb3cd14
Xeptox <50525411+Xeptox@users.noreply.github.com>
2026-02-25 15:46:25 +0200

    Add files via upload

---
+++ lesson-11-JS-homework.js
@@ -0,0 +1,27 @@
+let arr = [9, 2, 4, 1, 5, 2, 9, 1, 2, 0]
+printArray(arr)
+bubbleSort(arr)
+printArray(arr) // 0, 1, 1, 2, 2, 2, 4, 5, 9, 9
+
+
+function bubbleSort(arr) {
+    // TODO: Implement bubble sort algorithm
+    let varTmp = 0
+    for (let i = 0; i < arr.length ; i++) {
+        for (let j = 0; j < arr.length ; j++){
+            if (arr[i] < arr[j]) {
+                varTmp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = varTmp
+            }
+        }
+    }
+}
+
+function printArray(arr) {
+    console.log('==========')
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
+    console.log('==========')
+}
