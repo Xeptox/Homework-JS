c85796cf67952bc901ab33190b3515e972e74ce0
Xeptox <antonfadeevbiz@gmail.com>
2026-03-04 16:52:00 +0200

    123

--- Lesson-13-JS-Intro-4/Homework/homework.js
+++ Lesson-13-JS-Intro-4/Homework/homework.js
@@ -3,12 +3,35 @@ printArray(arr)
 // selectionSort(arr)
 // selectionSort(arr, (a, b) => b - a)
 // shuffle
-selectionSort(arr, () => Math.random() - 0.5)
+// selectionSort(arr, () => Math.random() - 0.5)
+selectionSort(arr, (a, b) => (a > b))
 printArray(arr)
 
 
-function selectionSort(arr, comparator = (a, b) => a - b) {
+function selectionSort(arr, comparator = (a, b) => a > b) {
     // TODO implement selection sort algorithm
+    let minVar = arr[0]
+    for ( let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+        if (comparator(minVar, arr[j])) {
+            minVar = arr[j]
+        }
+        if (comparator(minVar, arr[i])) {
+            a
+
+        }
+    }
+    }
+
+    // for (let j = 0; j < arr.length-1; j++) {
+    //     for (let i = 0; i < arr.length-1; i++) {
+    //             if (comparator(arr[i], minVar) > 0) {
+    //                 let tmpVar = arr[i]
+    //                 arr[i] = arr[i + 1]
+    //                 arr[i + 1] = tmpVar
+    //             }
+    //         }
+    //     }
 }
 
 
