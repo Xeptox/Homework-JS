8891ce0eaa1027ad3f9ba6a5349104177aaadf9d
Xeptox <antonfadeevbiz@gmail.com>
2026-03-12 18:40:09 +0200

    rd

--- Lesson-16-JS-events-1/animation.js
+++ Lesson-16-JS-events-1/animation.js
@@ -1,14 +1,27 @@
-const box =document.getElementById("box")
+const box = document.getElementById("box")
+const container = document.getElementById("container")
 let pos = 0
-const limit = clientWidth - boxWidth
-const t = setInterval(move, 15)
+const limit = container.clientWidth - box.clientWidth
+const start = container.clientWidth
+
+
+if (pos === start && pos < limit) {
+    setInterval(move, 15)
+}
+if (pos === limit && pos > start) {
+    setInterval(moveBack, 15)
+}
+
 function move() {
     //TODO
-    pos++
-    box.style.top = pos + "px"
-    box.style.left = pos + "px"
-    if (pos >= limit) {
-        clearInterval(t)
-
-    }
+        pos++
+        box.style.top = pos + "px"
+        box.style.left = pos + "px"
 }
+
+function moveBack() {
+        pos--
+        box.style.top = pos - "px"
+        box.style.left = pos - "px"
+    }
+
---
+++ TEST_2.js
@@ -0,0 +1,7 @@
+let counter = 0
+for ( let i = 85, sum = 0; sum <= 1505; i +=5) {
+    sum += i
+    console.log(sum)
+    counter++
+}
+console.log(counter)
