e9fb5139a624a1e390c52854020f69feeb3010a7
Anton <antonfadeevbiz@gmail.com>
2026-03-14 23:01:14 +0200

    ready

--- Lesson-16-JS-events-1/animation.js
+++ Lesson-16-JS-events-1/animation.js
@@ -2,26 +2,43 @@ const box = document.getElementById("box")
 const container = document.getElementById("container")
 let pos = 0
 const limit = container.clientWidth - box.clientWidth
-const start = container.clientWidth
+const start = 0
 
 
-if (pos === start && pos < limit) {
-    setInterval(move, 15)
-}
-if (pos === limit && pos > start) {
-    setInterval(moveBack, 15)
-}
+const t = setInterval(move, 150)
+// basic movement function
+// function move() {
+//     pos++
+//     box.style.top = pos + "px"
+//     box.style.left = pos + "px"
+//     if (pos === limit) {
+//         clearInterval(t)
+//     }
+// }
 
 function move() {
     //TODO
+    if (pos === start) {
         pos++
         box.style.top = pos + "px"
         box.style.left = pos + "px"
-}
-
-function moveBack() {
-        pos--
-        box.style.top = pos - "px"
-        box.style.left = pos - "px"
     }
+    if (pos > start) {
+        pos++
+        box.style.top = pos + "px"
+        box.style.left = pos + "px"
+        }
+    if ( pos === limit) {
+            pos--
+            box.style.top = pos + "px"
+            box.style.left = pos + "px"
+        }
+    if (pos < limit) {
+        pos--
+        box.style.top = pos + "px"
+        box.style.left = pos + "px"
+    }
+
+
+}
 
--- Lesson-16-JS-events-1/gallery.js
+++ Lesson-16-JS-events-1/gallery.js
@@ -1,12 +1,21 @@
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const picture = document.getElementById('picture')
-
+let slide = 1
 prev.onclick = function () {
     //TODO
-    picture.src = "Images/Rouen_Cathedral_1.jpg"
+    slide--
+    picture.src = `Images/Rouen_Cathedral_${slide}.jpg`
+    if (slide === 1){
+        slide = 6
+    }
 }
 next.onclick = function () {
     //TODO
-    picture.src = "Images/Rouen_Cathedral_2.jpg"
+    slide++
+    if (slide === 6){
+        slide = 1
+    }
+    picture.src = `Images/Rouen_Cathedral_${slide}.jpg`
+
 }
